f78c7f38a50a2dca688eb3711e674497383aa9f8
NoranAhmed <47926647+NoranAhmed@users.noreply.github.com>
2019-05-08 01:30:02 +0200

    Update Developer2.java
    the difference between stash and shelv

--- developer2/src/developer2/Developer2.java
+++ developer2/src/developer2/Developer2.java
@@ -19,7 +19,10 @@ public class Developer2 {
         int x =10 ; // first commit by aya
         int y =10; // second by aya
         int b = 1505;
+        string stash = " Stashing is when you want to record the current state of the working directory and the index, but want to go back to a clean working directory.It saves your local modifications away and reverts the working directory to match the HEAD commit.";
+        string shelv = "Shelving is temporarily storing pending changes that have not been committed yet.It's useful when you need to switch to another task and you want to set your changes aside to work on them later.";
     }
+   
     
     public void add(int x, int y)
     {
